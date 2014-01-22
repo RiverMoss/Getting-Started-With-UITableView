78425ca97dca9df2d10ff685044741ac40aa76ce
Nick Martin <nick@Nicks-MacBook-Pro.local>
2014-01-21 20:20:57 -0700

    Init Commit

--- TableViewDemo/ViewController.m
+++ TableViewDemo/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    NSArray *_myData;
+}
 
 @end
 
@@ -17,13 +19,50 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[self populateTableData];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)populateTableData{
+    _myData = @[@"Apple", @"Orange", @"Banana", @"Pear"];
+}
+
+#pragma mark - UITableViewDataSource delegate methods
+
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [_myData count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *MyIdentifier = @"MyReuseIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:MyIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault  reuseIdentifier:MyIdentifier];
+    }
+    cell.textLabel.text = [_myData objectAtIndex:indexPath.row];
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+#pragma mark - UITableViewDelegate delegate methods
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString *selectedFruit = [_myData objectAtIndex:indexPath.row];
+    NSString *myMessage = [NSString stringWithFormat:@"You selected: %@", selectedFruit];
+    UIAlertView *message = [[UIAlertView alloc]initWithTitle:@"Selected Fruit" message: myMessage
+                                           delegate:nil
+                                           cancelButtonTitle:@"OK"
+                                           otherButtonTitles:nil];
+    [message show];
 }
 
 @end
